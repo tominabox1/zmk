97ee58b9fa1ddbc85dfea280416b040dd2c3f070
Nick <nick.win999@gmail.com>
2020-11-08 14:40:47 -0600

    feat: Turn off external power on sleep

--- app/src/power.c
+++ app/src/power.c
@@ -16,6 +16,7 @@ LOG_MODULE_DECLARE(zmk, CONFIG_ZMK_LOG_LEVEL);
 #include <zmk/event-manager.h>
 #include <zmk/events/position-state-changed.h>
 #include <zmk/events/sensor-event.h>
+#include <drivers/ext_power.h>
 
 static u32_t power_last_uptime;
 
@@ -35,6 +36,20 @@ enum power_states sys_pm_policy_next_state(s32_t ticks) {
     s32_t current = k_uptime_get();
     if (power_last_uptime > 0 && !is_usb_power_present() &&
         current - power_last_uptime > MAX_IDLE_MS) {
+
+#if IS_ENABLED(CONFIG_ZMK_EXT_POWER)
+        struct device *ext_power = device_get_binding("EXT_POWER");
+        if (ext_power == NULL) {
+            LOG_ERR("Unable to retrieve ext_power device: EXT_POWER");
+        } else {
+            int rc = ext_power_disable(ext_power);
+
+            if (rc != 0) {
+                LOG_ERR("Unable to disable ext_power device: %d", rc);
+            }
+        }
+#endif /* CONFIG_ZMK_EXT_POWER */
+
         return SYS_POWER_STATE_DEEP_SLEEP_1;
     }
 #endif /* CONFIG_HAS_SYS_POWER_STATE_DEEP_SLEEP_1 */
